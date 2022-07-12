f595b7de54600b0f7d3ab6f5f01e7ea40d2e4e96
wwWSerGWww <wwwsergwww@mail.ru>
2022-07-12 15:18:28 +0300

    Add fix to logger

--- source/utils/index.js
+++ source/utils/index.js
@@ -3,9 +3,9 @@ export { getPort } from './env/index.js'
 export { getPassword } from './env/index.js'
 // export { sessionOptions } from './options/index.js'
 export { authorization } from './authorization.js'
-export { logger } from './logger.js'
+export { logger } from './logger/logger.js'
 export {
   errorLogger,
   validationErrorLogger,
   notFoundErrorLogger,
-} from './loggerError.js'
+} from './logger/loggerError.js'
